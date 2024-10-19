4904b6423e2fd643aede4bf60adda0bfa88d6043
Yanna Katryny Carneiro Nobrega <yanann1997@gmail.com>
2024-10-19 00:15:38 -0300

    ProductController implemented

--- src/main/java/br/com/catalogproduct/catalog_product/adapters/in/rest/ProductController.java
+++ src/main/java/br/com/catalogproduct/catalog_product/adapters/in/rest/ProductController.java
@@ -1,9 +1,15 @@
 package br.com.catalogproduct.catalog_product.adapters.in.rest;
 
 import br.com.catalogproduct.catalog_product.adapters.out.ProductServicepPortOutImpl;
+import br.com.catalogproduct.catalog_product.domain.dto.ProductRequest;
+import br.com.catalogproduct.catalog_product.domain.dto.ProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -12,4 +18,29 @@ public class ProductController {
 
     @Autowired
     private ProductServicepPortOutImpl service;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(ProductRequest productRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(productRequest));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<ProductResponse>> updateProduct(@RequestBody ProductRequest productRequest, @PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(productRequest, id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<ProductResponse>> getProductById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
